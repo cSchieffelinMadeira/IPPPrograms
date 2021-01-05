7558f366edbcf323837341e7b26ffd0709787000
cSchieffelinMadeira <76934842+cSchieffelinMadeira@users.noreply.github.com>
2021-01-05 10:24:07 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# statement 1
+user_name = input("what is your name?")
+print(user_name)
+
+# statement 2
+user_age = input("What is your age?")
+print(user_age)
+
+# statement 3
+user_song = input("What is your favorite song?")
+print(user_song)
+
+# statement 4
+user_home = input("Where do you live?")
+print(user_home)
+
+# statement 5
+user_pets = input("How many pets do you have?")
+print(user_pets)
+
+
